16b07f91a41f46fe4a9c2afb2ae41fce79aa34e7
Olamide-Odusote <olamidewodusote@gmail.com>
2026-07-22 15:09:28 +0100

    Changed title for login, register and board pages

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $board->name }}</title>
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
